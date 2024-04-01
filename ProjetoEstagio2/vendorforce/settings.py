3b3d13ee92b5a272c4b7e00baa4bc22bb8754925
Kenzo <kenzoft20@gmail.com>
2024-03-31 22:52:00 -0300

    gunicorn adicionado

--- ProjetoEstagio2/vendorforce/settings.py
+++ ProjetoEstagio2/vendorforce/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-gdju0b1@hw75v8g%h-8)p^sm561=logzndz#*b0@uz1!b8t)^8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
