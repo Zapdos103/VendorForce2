dc5f95584de7bca6ab8efdff24b21d023c8591b7
Kenzo Fagundes Tabuchi <134327714+Zapdos103@users.noreply.github.com>
2024-11-18 15:31:16 -0300

    Update settings.py

--- ProjetoEstagio2/vendorforce/settings.py
+++ ProjetoEstagio2/vendorforce/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gdju0b1@hw75v8g%h-8)p^sm561=logzndz#*b0@uz1!b8t)^8'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
